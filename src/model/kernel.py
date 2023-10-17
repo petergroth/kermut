8960894d10c9ad91b8b73ed9bcfad78e4758e4e8
Peter Mørch Groth <petermoercgroth@gmail.com>
2023-10-17 13:48:39 +0200

    refactoring and bugfixing

--- src/model/kernel.py
+++ src/model/kernel.py
@@ -191,101 +191,124 @@ class KermutHellingerKernelMulti(Kernel):
         self,
         conditional_probs: torch.Tensor,
         wt_sequence: torch.LongTensor,
-        p_B: float = 1.0,
-        p_Q: float = 1.0,
+            p_B: float = 15.0,
+            p_Q: float = 5.0,
         theta: float = 1.0,
         gamma: float = 1.0,
         learnable_transform: bool = False,
         learnable_hellinger: bool = False,
     ):
         super(KermutHellingerKernelMulti, self).__init__()
+        self.learnable_transform = learnable_transform
+        self.learnable_hellinger = learnable_hellinger
+
+        # If learnable, pass parameters through softplus function to ensure positivity during learning
         if learnable_transform:
             self.register_parameter(
-                name="p_B", parameter=torch.nn.Parameter(torch.tensor(p_B))
+                name="_p_B", parameter=torch.nn.Parameter(torch.tensor(p_B))
             )
             self.register_parameter(
-                name="p_Q", parameter=torch.nn.Parameter(torch.tensor(p_Q))
+                name="_p_Q", parameter=torch.nn.Parameter(torch.tensor(p_Q))
             )
             self.transform_fn = nn.Softplus()
         else:
             assert p_B > 0 and p_Q > 0
-            self.p_B = torch.tensor(p_B)
-            self.p_Q = torch.tensor(p_Q)
+            self.register_buffer("_p_B", torch.tensor(p_B))
+            self.register_buffer("_p_Q", torch.tensor(p_Q))
             self.transform_fn = nn.Identity()
 
         if learnable_hellinger:
             self.register_parameter(
-                name="theta", parameter=torch.nn.Parameter(torch.tensor(theta))
+                name="_theta", parameter=torch.nn.Parameter(torch.tensor(theta))
             )
             self.register_parameter(
-                name="gamma", parameter=torch.nn.Parameter(torch.tensor(gamma))
+                name="_gamma", parameter=torch.nn.Parameter(torch.tensor(gamma))
             )
             self.hellinger_fn = nn.Softplus()
         else:
             assert theta > 0 and gamma > 0
-            self.theta = torch.tensor(theta)
-            self.gamma = torch.tensor(gamma)
+            self.register_buffer("_theta", torch.tensor(theta))
+            self.register_buffer("_gamma", torch.tensor(gamma))
             self.hellinger_fn = nn.Identity()
 
         self.conditional_probs = conditional_probs
         self.hellinger = hellinger_distance(conditional_probs, conditional_probs)
         self.wt_sequence = wt_sequence
+        self.register_buffer("conditional_probs", conditional_probs)
+            "hellinger", hellinger_distance(conditional_probs, conditional_probs)
+        )
+        self.register_buffer("wt_sequence", wt_sequence)
 
     def forward(self, x1: torch.Tensor, x2: torch.Tensor, **kwargs):
         # Indices where x1 and x2 differ to the WT. First column is batch, second is position.
         x1_idx = torch.argwhere(x1 != self.wt_sequence)
         x2_idx = torch.argwhere(x2 != self.wt_sequence)
 
-        # Create all pairs of indices
-        x1_expanded = x1_idx.unsqueeze(1).expand(x1_idx.size(0), x2_idx.size(0), -1)
-        x2_expanded = x2_idx.unsqueeze(0).expand(x1_idx.size(0), x2_idx.size(0), -1)
-        all_pairs = torch.cat((x1_expanded, x2_expanded), dim=2)
-        all_pairs = all_pairs.view(-1, 4)
-        # Extract batch and position indices
-        batch_idx = all_pairs[:, [0, 2]]  # First column x1, second column x2
-        pos_idx = all_pairs[:, [1, 3]]  # First column x1, second column x
+        # Get batch and position indices of all mutation pairs (> N*N)
+        batch_idx_x1 = x1_idx[:, 0].repeat_interleave(x2_idx[:, 0].shape[0])
+        batch_idx_x2 = x2_idx[:, 0].repeat(x1_idx[:, 0].shape[0])
+        pos_idx_x1 = x1_idx[:, 1].repeat_interleave(x2_idx[:, 1].shape[0])
+        pos_idx_x2 = x2_idx[:, 1].repeat(x1_idx[:, 1].shape[0])
 
         # Extract and transform Hellinger distances
-        hn = self.hellinger[pos_idx[:, 0], pos_idx[:, 1]]
-        k_hn = self.hellinger_fn(self.theta) * torch.exp(
-            -self.hellinger_fn(self.gamma) * hn
-        )
+        hn = self.hellinger[pos_idx_x1, pos_idx_x2]
+        k_hn = self.theta * torch.exp(-self.gamma * hn)
 
         # Extract conditional probabilities
-        x1_toks = x1[batch_idx[:, 0], pos_idx[:, 0]]
-        x2_toks = x2[batch_idx[:, 1], pos_idx[:, 1]]
-        p_x1 = self.conditional_probs[pos_idx[:, 0], x1_toks]
-        p_x2 = self.conditional_probs[pos_idx[:, 1], x2_toks]
-        k_p_x1x2 = 1 / (
-            1
-            + self.transform_fn(self.p_Q)
-            * torch.exp(-self.transform_fn(self.p_B) * p_x1 * p_x2)
-        )
+        x1_toks = x1[batch_idx_x1, pos_idx_x1]
+        x2_toks = x2[batch_idx_x2, pos_idx_x2]
+        p_x1 = self.conditional_probs[pos_idx_x1, x1_toks]
+        p_x2 = self.conditional_probs[pos_idx_x2, x2_toks]
+        k_p_x1x2 = 1 / (1 + self.p_Q * torch.exp(-self.p_B * p_x1 * p_x2))
 
-        # Sum kernel values for each sequence pair
-        output = torch.zeros(x1.size(0), x2.size(0))
         # Get unique indices and original placement
         unique_indices, inverse_indices = torch.unique(
-            batch_idx, return_inverse=True, dim=0
+            torch.cat((batch_idx_x1.unsqueeze(1), batch_idx_x2.unsqueeze(1)), -1),
+            return_inverse=True,
+            dim=0,
         )
         unique_indices = unique_indices.long()
         k_sum = torch.zeros(len(unique_indices))
         k_sum = torch.scatter_add(k_sum, 0, inverse_indices, k_hn * k_p_x1x2)
+        output = torch.zeros(x1.size(0), x2.size(0))
         output[unique_indices[:, 0], unique_indices[:, 1]] = k_sum
-
         return output
 
     def get_params(self) -> dict:
         return {
-            "p_B": self.transform_fn(self.p_B).item(),
-            "p_Q": self.transform_fn(self.p_Q).item(),
-            "theta": self.hellinger_fn(self.theta).item(),
-            "gamma": self.hellinger_fn(self.gamma).item(),
+            "p_B": self.p_B.item(),
+            "p_Q": self.p_Q.item(),
+            "theta": self.theta.item(),
+            "gamma": self.gamma.item(),
         }
+
+    @property
+    def p_B(self):
+        if self.learnable_transform:
+            return self.transform_fn(self._p_B)
+        return self._p_B
+
+    @property
+    def p_Q(self):
+        if self.learnable_transform:
+            return self.transform_fn(self._p_Q)
+        return self._p_Q
+
+    @property
+    def theta(self):
+        if self.learnable_hellinger:
+            return self.hellinger_fn(self._theta)
+        return self._theta
+
+    @property
+    def gamma(self):
+        if self.learnable_hellinger:
+            return self.hellinger_fn(self._gamma)
+        return self._gamma
 
 
 if __name__ == "__main__":
-    dataset = "BLAT_ECOLX"
+    dataset = "GFP"
     conditional_probs_path = Path(
         "data",
         "interim",
@@ -301,19 +324,37 @@ if __name__ == "__main__":
         conditional_probs_path, as_tensor=True
     )
     df = pd.read_csv(assay_path, sep="\t")
+    df = df[df["n_muts"] <= 2]
+    df = df.iloc[:1000]
+    y = df["delta_fitness"].values
+    y = torch.tensor(y)
 
     tokenizer = Tokenizer()
     sequences = df["seq"]
     tokens = tokenizer(sequences)
-    wt_sequence = tokenizer([BLAT_ECOLX_WT])[0]
+    wt_sequence = tokenizer([GFP_WT])[0]
 
-    # TODO: Compute distance between two sequences
-    # TODO: Batched version
+    model_kwargs = {
+        "conditional_probs": conditional_probs,
+        "wt_sequence": wt_sequence,
+        "p_B": 15.0,
+        "p_Q": 5.0,
+        "theta": 1.0,
+        "gamma": 1.0,
+        "learnable_transform": False,
+        "learnable_hellinger": False,
+    }
 
-    x1 = tokens[[0, 30, 100]]
-    x2 = tokens[[0, 30, 100]]
+    kernel = KermutHellingerKernelMulti(**model_kwargs)
 
-    kernel = KermutHellingerKernelMulti(
-        conditional_probs=conditional_probs, wt_sequence=wt_sequence
-    )
-    tmp = kernel(tokens[:100]).evaluate()
+    # FOR PROFILING
+    # from torch.profiler import profile, record_function, ProfilerActivity
+
+    # with profile(
+    #     activities=[ProfilerActivity.CPU], profile_memory=True, record_shapes=True
+    # ) as prof:
+    #     with record_function("model_inference"):
+    out = kernel(tokens)  # IF PROFILING, INDENT
+
+    # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+    # print(prof.key_averages().table(sort_by="self_cpu_memory_usage", row_limit=10))
