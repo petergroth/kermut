79d3a813320380368d83c2b4b0c9d21a69fca37f
Peter Mørch Groth <petermoercgroth@gmail.com>
2023-10-24 16:27:49 +0200

    fix a few bugs

--- src/data/conditional_probabilities.py
+++ src/data/conditional_probabilities.py
@@ -5,12 +5,8 @@ import torch
 from esm import pretrained
 import pandas as pd
 from tqdm import trange
-from Bio import SeqIO
-import itertools
-from typing import List, Tuple
-import numpy as np
 from pathlib import Path
-from src import AA_TO_IDX
+from src import ALPHABET
 
 
 def compute_ESM_masked_plls(dataset: str):
@@ -33,21 +29,24 @@ def compute_ESM_masked_plls(dataset: str):
     data = [("wt", wt_sequence)]
     batch_labels, batch_strs, batch_tokens = batch_converter(data)
 
-    # Compute masked pseudo-probabilities at each position conditioned on the rest
-    log_probs = torch.zeros((len(wt_sequence), len(alphabet)))
-    for i in trange(1, len(wt_sequence) + 1):
-        batch_tokens_masked = batch_tokens.clone()
-        batch_tokens_masked[0, i] = alphabet.mask_idx
-        if torch.cuda.is_available():
-            batch_tokens_masked = batch_tokens_masked.cuda()
-        with torch.no_grad():
-            token_probs = torch.softmax(model(batch_tokens_masked)["logits"], dim=-1)
-        log_probs[i - 1] = token_probs[0, i]
+    with torch.no_grad():
+        # Compute masked pseudo-probabilities at each position conditioned on the rest
+        log_probs = torch.zeros((len(wt_sequence), len(alphabet)))
+        for i in trange(1, len(wt_sequence) + 1):
+            batch_tokens_masked = batch_tokens.clone()
+            batch_tokens_masked[0, i] = alphabet.mask_idx
+            if torch.cuda.is_available():
+                batch_tokens_masked = batch_tokens_masked.cuda()
+            with torch.no_grad():
+                token_probs = torch.softmax(
+                    model(batch_tokens_masked)["logits"], dim=-1
+                )
+            log_probs[i - 1] = token_probs[0, i]
 
-    # Alphabet reordering to align with Kermut
-    order = [AA_TO_IDX[aa] for aa in alphabet.all_toks if aa in AA_TO_IDX.keys()]
-    p = log_probs[:, order].clone()
-    torch.save(p, output_path)
+        # Alphabet reordering to align with Kermut
+        order = [alphabet.tok_to_idx[aa] for aa in ALPHABET]
+        p = log_probs[:, order].clone()
+        torch.save(p, str(output_path))
 
 
 if __name__ == "__main__":
